2a14068bf518a2906c3386fdcab655fbb75dcd49
CHAMBARD Florian Jacky <florian.chambard@d-lmd.local>
2023-03-31 16:01:53 +0200

    Création de l'ouverture de la fenêtre et gestion de mode jeu/dev

---
+++ sevenlives/game.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pygame
+import sys
+
+class TheSevenLives:
+    def __init__(self,titre,mode="jeu"):
+        assert mode=="dev" or mode=="jeu"
+        
+        pygame.init()
+        
+        if mode=="dev":
+            pygame.display.set_mode((1024,576))
+        else:
+            pygame.display.set_mode()
+        
+        pygame.display.set_caption(titre)
+        
+        self.clock=pygame.time.Clock()
+    
+    def update(self,delta_time):
+        pass
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+        
+            delta_time=self.clock.tick()
+            self.update(delta_time)
+            pygame.display.update()

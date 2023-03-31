71dd682e13f7c1596b836a112f4ab78d5e8a8dc7
Xibitol <axel@ad-micro.fr>
2023-03-31 15:04:30 +0200

    Updated pygame.ps1

--- bin/pygame.ps1
+++ bin/pygame.ps1
@@ -1,4 +1,4 @@
 # This script install pygame for Lycee Marcel Dassault computers
 Write-Output "Installation of Pygame ...";
 (New-Object -comObject WScript.Shell).Exec("C:\Nsi\Anaconda3\Scripts\pip.exe install pygame -U")
-pause
+exit

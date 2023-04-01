524033c1f4034f981f640a8777440617c4ee5385
Xibitol <axel@ad-micro.fr>
2023-04-01 21:01:36 +0200

    Remaked scripts correcly and with windows cmd

---
+++ bin/highschool-install-pygame.cmd
@@ -0,0 +1,5 @@
+@echo off
+rem This script install pygame python package for a Lycee Marcel Dassault's computer
+
+C:\Nsi\Anaconda3\Scripts\pip.exe install pygame -U
+pause
--- bin/pygame.ps1
+++
@@ -1,4 +0,0 @@
-# This script install pygame for Lycee Marcel Dassault computers
-Write-Output "Installation of Pygame ...";
-(New-Object -comObject WScript.Shell).Exec("C:\Nsi\Anaconda3\Scripts\pip.exe install pygame -U")
-exit

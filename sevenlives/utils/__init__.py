8bd91c04643f46306297ec52d1bf6aa92556aec0
Xibitol <axel@ad-micro.fr>
2023-04-22 22:16:54 +0200

    Fixed Utils: project assets path

--- sevenlives/utils/__init__.py
+++ sevenlives/utils/__init__.py
@@ -6,14 +6,14 @@ __author__ = "Xibitol"
 
 import os, pygame
 
-PROJECT_ROOT = "sevenlives"
+PROJECT_ASSETS_ROOT = "sevenlives/assets"
 
 def getAssetFolder(subpackage: str, element: str = None):
     assert any(
-        [subpackage == d for d in filter(lambda f: f != "__pycache__", os.listdir(PROJECT_ROOT))]
+        [subpackage == d for d in filter(lambda f: f != "__pycache__", os.listdir(PROJECT_ASSETS_ROOT))]
     ), "You have to give a real subpackage name."
 
-    spRoot = os.path.join(PROJECT_ROOT, subpackage)
+    spRoot = os.path.join(PROJECT_ASSETS_ROOT, subpackage)
     return os.path.join(spRoot, element) if element != None else spRoot
 
 def ScrW() -> int:

c42eaad6e046650d6c2124c7fa7d3be3b5d2461d
Xibitol <axel@ad-micro.fr>
2023-04-22 23:03:54 +0200

    Added logo.

--- sevenlives/level/mainLevel.py
+++ sevenlives/level/mainLevel.py
@@ -1,4 +1,4 @@
-import pygame
+import os, pygame
 from sevenlives.utils import ScrW, ScrH
 from sevenlives.level import Level
 from sevenlives.interface import Button
@@ -6,19 +6,25 @@ from sevenlives.interface import Button
 class MainLevel:
     def __init__(self):
         self.__parent = Level("menu")
-
-        origin = pygame.Vector2(ScrW()*2/3, ScrH()/2)
         self._buttons: list[Button] = [
-            Button("start", origin, self.startClicked),
-            Button("continue", origin, self.continueClicked),
-            Button("quit", origin, self.quitClicked),
+            Button("start", pygame.Vector2(0, 0), self.startClicked),
+            Button("continue", pygame.Vector2(0, 0), self.continueClicked),
+            Button("quit", pygame.Vector2(0, 0), self.quitClicked),
         ]
 
-        space = 40
-        origin = origin - pygame.Vector2(0, self._buttons[0]._rect.h)
-        for (i, button) in enumerate(self._buttons):
-            button._rect.topleft = self._buttons[i - 1]._rect.bottomleft + pygame.Vector2(0, space) if i > 0 else origin
+        space = pygame.Vector2(0, 40)
+        origin = pygame.Vector2(ScrW()*2/3, ScrH()/2 - self._buttons[0]._rect.h)
 
+        # Logo
+        self._logo = pygame.image.load(os.path.join(self.__parent.getPath(), "logo.png"))
+        self._logoRect = self._logo.get_rect()
+        self._logoRect.bottomleft = origin - space - pygame.Vector2((self._logoRect.w - self._buttons[0]._rect.w)/2, 0)
+
+        # Buttons
+        for (i, button) in enumerate(self._buttons):
+            button._rect.topleft = self._buttons[i - 1]._rect.bottomleft + space if i > 0 else origin
+
+        # Credits
         self._cdtText = pygame.font.SysFont("Arial", 15).render(
             "The Seven Lives - A NSI project by Xibitol, Nagisou and Mélody - licensed under GNU GPL v3.",
             True,
@@ -40,6 +46,7 @@ class MainLevel:
             button.draw(surface)
 
         surface.blit(self._cdtText, self._cdtRect)
+        surface.blit(self._logo, self._logoRect)
 
     # Buttons actions
     def startClicked(self):

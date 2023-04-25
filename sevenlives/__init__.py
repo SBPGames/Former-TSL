0779b384f7bc992c9222a7ca33697e0c7c691a7a
Xibitol <axel@ad-micro.fr>
2023-04-25 08:55:55 +0200

    Added Mouse updating and Fixed level init.

--- sevenlives/__init__.py
+++ sevenlives/__init__.py
@@ -5,11 +5,11 @@ from typing import Any
 import pygame
 from sevenlives.mode import Mode
 from sevenlives.level import MainLevel
+from sevenlives.interface import Mouse
 
 class TheSevenLives:
     def __init__(self, mode: Mode = Mode.PRODUCTION):
         self._mode = mode
-        self._level = MainLevel()
         pygame.init()
 
         if self._mode == Mode.DEVELOPMENT: 
@@ -22,6 +22,7 @@ class TheSevenLives:
 
         pygame.display.set_caption("7lives")
 
+        self._level = MainLevel()
         self.clock = pygame.time.Clock()
 
     def setLevel(self, level):
@@ -34,6 +35,8 @@ class TheSevenLives:
                     pygame.quit()
                     break
             else:
+                Mouse.update()
+
                 if self._level != None:
                     self._level.update(self.clock.tick())
                     self._level.draw(self._surface)
